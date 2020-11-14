216715cd5882ab2819d17a9889248a34485a71ef
DEll <vutranquochunghau@gmail.com>
2020-11-14 09:26:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/e_commerceadmin/Activity/Login_Admin.java
@@ -0,0 +1,16 @@
+package com.example.e_commerceadmin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_commerceadmin.R;
+
+public class Login_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__admin);
+    }
+}

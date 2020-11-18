efa0c8df99db0855716de2822caa3fe419d67b99
DEll <vutranquochunghau@gmail.com>
2020-11-18 12:05:51 +0700

    ProjectMain.hau

---
+++ app/src/main/java/com/example/e_commerceadmin/Activity/Trangchu_Admin.java
@@ -0,0 +1,18 @@
+package com.example.e_commerceadmin.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_commerceadmin.R;
+
+public class Trangchu_Admin extends AppCompatActivity {
+
+    private static final android.R.attr R = new android.R.attr();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.e_commerceadmin.R.layout.activity_trangchu__admin);
+    }
+}

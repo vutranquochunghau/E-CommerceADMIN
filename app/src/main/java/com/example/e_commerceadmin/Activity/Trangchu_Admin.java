4d3e2f911d1f293a32c560d26a86433379c984ae
DEll <vutranquochunghau@gmail.com>
2020-11-19 10:45:24 +0700

    ProjectMain.hau

--- app/src/main/java/com/example/e_commerceadmin/Activity/Trangchu_Admin.java
+++ app/src/main/java/com/example/e_commerceadmin/Activity/Trangchu_Admin.java
@@ -1,18 +1,31 @@
 package com.example.e_commerceadmin.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.e_commerceadmin.R;
 
 public class Trangchu_Admin extends AppCompatActivity {
 
-    private static final android.R.attr R = new android.R.attr();
+
+//    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.e_commerceadmin.R.layout.activity_trangchu__admin);
     }
+
+//    public void clickMenu(View view){
+//        openDrawer(drawerLayout);
+//    }
+//
+//    private static void openDrawer(DrawerLayout drawerLayout){
+//        drawerLayout.openDrawer(GravityCompat.START);
+//    }
 }
